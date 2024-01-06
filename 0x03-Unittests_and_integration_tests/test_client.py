9f2427a75cf075633f89edcf555b49d10ede2e85
uelkariuki <cratzuel@gmail.com>
2024-01-06 19:59:25 +0300

    Parameterize the unit test utils.access_nested_map

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     @parameterized.expand([
         ({"a": 1}, "a", 1),
-        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2,)
     ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
