f169abadc92f59aa6d8ff22772c685373c070b4b
uelkariuki <cratzuel@gmail.com>
2024-01-07 06:35:21 +0300

    Mock HTTP calls with unittest.mock.patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
 """Parameterize a unit test"""
 
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Any, Mapping, Sequence
 
 
@@ -29,3 +30,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test KeyError is raised for the unittest"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """
+    class with unit tests for utils.get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Test that utils.get_json returns expected result"""
+        mock_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
