5fc274733e279a5cab918ef6c9eeb9bbb0fe388e
uelkariuki <cratzuel@gmail.com>
2024-01-07 15:15:42 +0300

    Test that GithubOrgClient.org returns the correct value

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-"""Parameterize a unit test"""
+"""Unittests for client.GithubOrgClient class"""
 
 import unittest
+from unittest.mock import patch
+from client import GithubOrgClient, get_json
 from parameterized import parameterized
-from utils import access_nested_map
-from typing import Any, Mapping, Sequence
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """
-    Class with Unit tests for utils.access_nested_map
-    """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
-        """ Test access nested map"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+class TestGithubOrgClient(unittest.TestCase):
+	"""Unittests for GithubOrgClient class"""
+
+	@parameterized.expand([
+		("google",),
+		("abc",)
+	])
+	@patch('client.get_json', return_value={"key":"value"})
+	def test_org(self, org_name, mock_get_json):
+		"""Method to ensure org method works as expected"""
+		test_client = GithubOrgClient(org_name)
+		result = test_client.org
+		self.assertEqual(result, {"key":"value"})
+		mock_get_json.assert_called_once()
+
